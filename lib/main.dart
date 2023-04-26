d019fe15209a41606e048480212a6245f8861c7c
GirlWhoCodes14 <82748504+GirlWhoCodes14@users.noreply.github.com>
2023-04-26 14:40:32 +0800

    login page with authentication
    can only sign it with the following email and password
    
    email: alexisayuso12@gmail.com
    password: test123
    
    MISSING: Google sign in authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart';
+import 'ui/pages/auth_page.dart';
+import 'ui/theme.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    //options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  //const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "NDHUlite Login UI",
-      home: Scaffold(body: const LoginPage(),),
+      title: "NDHUlite Login",
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      //themeMode: ThemeService().theme,
+      home: Scaffold(body: AuthPage(),),
     );
   } // Widget build...
 }
